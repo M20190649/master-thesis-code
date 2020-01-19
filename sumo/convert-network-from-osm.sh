fff5b063967908fefe6ba7dd22be23e5e4dd0620
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-01-19 21:27:58 +0100

    add recommended options

--- sumo/convert-network-from-osm.sh
+++ sumo/convert-network-from-osm.sh
@@ -5,5 +5,5 @@ cd ../sumo
 
 echo
 echo "Converting network..."
-netconvert --osm-files ../osm/berlin-latest.osm.xml -o osm-network.net.xml
+netconvert --osm-files ../osm/berlin-latest.osm.xml -o osm-network.net.xml --geometry.remove --ramps.guess --junctions.join --tls.guess-signals --tls.discard-simple --tls.join --roundabouts.guess
 echo "Done!"
