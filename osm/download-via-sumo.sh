2b0c927ca4ab76f8ea37a767d7e5697d2be155ca
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-01-20 21:40:33 +0100

    fix merge conflict

--- osm/download-via-sumo.sh
+++ osm/download-via-sumo.sh
@@ -1,8 +1,9 @@
 FILE_NAME=greater-berlin
 
-# echo "Downloading OSM Data..."
+echo "Downloading OSM Data..."
 # python $SUMO_HOME/tools/osmGet.py --bbox 12.9529,52.2942,13.9018,52.7429 --prefix $FILE_NAME
-# echo "Done"
+python $SUMO_HOME/tools/osmGet.py --bbox 13.1561,52.3949,13.6780,52.6030 --prefix $FILE_NAME
+echo "Done"
 
 echo
 
