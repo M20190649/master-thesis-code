7fbb41393ef07c8e4b40055aac85072a7c7c8424
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-01-26 22:33:48 +0100

    rename file

--- scripts/convert-geojson-for-sumo.sh
+++ scripts/convert-geojson-for-sumo.sh
@@ -2,7 +2,7 @@ cd ..
 
 mkdir tmp
 
-node scripts/geojson-converter.js --input sumo/test-boundary.geojson --output tmp/polygon.xml
+node scripts/geojson2poly.js --input sumo/test-boundary.geojson --output tmp/polygon.xml
 
 polyconvert --xml-files tmp/polygon.xml --net-file sumo/osm-network.net.xml
 
