1393001b07492c96d0719ee5af403556545d71d0
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-01-26 22:34:44 +0100

    update queries. add command line args.

--- osm/downloadFromOverpass.js
+++ osm/downloadFromOverpass.js
@@ -1,15 +1,25 @@
 const fs = require("fs")
 const axios = require("axios")
+const commandLineArgs = require("command-line-args")
 
-const query = `(way["highway"](south,west,north,east);relation["highway"](south,west,north,east);)->.all;(way["highway"~"service|elevator|footway|steps|cycleway|corridor|path|pedestrian|platform"](south,west,north,east);)->.exclude;(.all; - .exclude;);out body;>;out skel qt;`
+const optionDefinitions = [
+  {
+    name: "bbox",
+    type: bboxString => bboxString.split(",").map(Number),
+  },
+  { name: "name", alias: "n", type: String },
+]
+const options = commandLineArgs(optionDefinitions)
 
-const testBbox = [52.5051, 13.3077, 52.5177, 13.3441] // south,west,north,east
+// const query = `(way["highway"](south,west,north,east);relation["highway"](south,west,north,east);)->.all;(way["highway"~"service|elevator|footway|steps|cycleway|corridor|path|pedestrian|platform"](south,west,north,east);way["highway"~"service"](south,west,north,east);)->.exclude;(.all; - .exclude;);out body;>;out skel qt;`
+const query = `(way["highway"~"motorway|trunk|primary|secondary|tertiary|residential|living_street|motorway_link|motorway_link|trunk_link|primary_link|secondary_link|tertiary_link|road|unclassified"](south,west,north,east);>;);out;`
+
+const testBbox = [52.5056, 13.3075, 52.5182, 13.344] // south,west,north,east
 const greaterBerlinBbox = [52.2984, 12.8815, 52.7001, 14.0474] // south,west,north,east
 
-const bbox = greaterBerlinBbox
+const bbox = options.bbox || testBbox
 
-const outputStream = fs.createWriteStream("./berlin-roads.osm.xml")
-
+const outputStream = fs.createWriteStream(`./${options.name || "road-network"}.osm.xml`)
 axios
   .get("http://overpass-api.de/api/interpreter", {
     responseType: "stream",
