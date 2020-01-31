743cd014f04061d4adae29225a5173e12b4b0e74
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-02-01 00:45:52 +0100

    convert to js

--- scripts/convert-osm-network.sh
+++
@@ -1,5 +0,0 @@
-cd ../sumo
-
-echo "Converting network..."
-netconvert --osm-files ../osm/ernst-reuter.osm.xml -o osm-network.net.xml --geometry.remove --ramps.guess --junctions.join --tls.guess-signals --tls.discard-simple --tls.join --roundabouts.guess
-echo "Done!"
