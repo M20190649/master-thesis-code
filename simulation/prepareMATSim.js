3f2e06d9c29703950a1139539db3e555b44abaf8
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-02-19 00:31:34 +0100

    move variables. always use pre-converted net. add exist check to every step.

--- simulation/prepareMATSim.js
+++ simulation/prepareMATSim.js
@@ -7,22 +7,17 @@ const visualizeRoutes = require("../sumo/visualizeRoutes")
 
 const convertPlansToTrips = require("../matsim/convertPlansToTrips")
 
-const rootDir = join(__dirname, "..")
-const inputDir = join(rootDir, "simulation", "input")
-const matsimDir = join(rootDir, "matsim")
-const sumoDir = join(rootDir, "sumo")
-
-if (!fs.existsSync(inputDir)) {
-  fs.mkdirSync(inputDir)
-}
-
 module.exports = async (config = {}) => {
-  const networkName = `${config.networkName || "matsim"}-network`
-  const routesName = `${config.routesName || "matsim"}-routes`
+  const rootDir = join(__dirname, "..")
+  const inputDir = join(rootDir, "simulation", config.name || "input")
+  const matsimDir = join(rootDir, "matsim")
+  const sumoDir = join(rootDir, "sumo")
+
+  const networkName = `matsim-network`
+  const routesName = `matsim-routes`
 
   const matsimNetworkFile = join(matsimDir, "network", "berlin-v5-network.xml")
-  const defaultConvertedNetwork = join(matsimDir, "network", "berlin-v5-network-converted.net.xml")
-  let networkFile = `${join(inputDir, networkName)}.net.xml`
+  const networkFile = join(matsimDir, "network", "berlin-v5-network-converted.net.xml")
   const matsimPlans = join(matsimDir, "plans", `berlin-v5.4-${config.scenario}.output_plans.xml`)
   const tripsFile = `${join(inputDir, routesName)}.trips.xml`
   const routesFile = `${join(inputDir, routesName)}.rou.xml`
@@ -31,10 +26,9 @@ module.exports = async (config = {}) => {
   // 1. Prepare network data
   console.log("------------ Prepare Network Data ------------")
 
-  // Convert OSM network to SUMO network
+  // Convert MATSim network to SUMO network
   console.log("Converting MATSim network to SUMO network...")
-  if (fs.existsSync(defaultConvertedNetwork)) {
-    networkFile = defaultConvertedNetwork
+  if (fs.existsSync(networkFile)) {
     console.log("Converted network already exists")
   } else {
     await convertMATSimNetwork({
@@ -49,29 +43,44 @@ module.exports = async (config = {}) => {
   console.log("------------ Prepare Routes Data ------------")
   // Parse all the plans for the bbox and convert them to trips
   console.log("Converting the MATSim plans...")
-  await convertPlansToTrips({
-    plans: matsimPlans,
-    mode: "matsim",
-    output: tripsFile,
-  })
+  if (fs.existsSync(tripsFile)) {
+    console.log("Trips file already exists")
+  } else {
+    await convertPlansToTrips({
+      plans: matsimPlans,
+      mode: "matsim",
+      output: tripsFile,
+    })
+  }
+
   console.log("Done!\n")
 
   // Convert trips into SUMO routes with the SUMO network
   console.log("Converting trips into SUMO routes...")
-  await convertTripsToRoutes({
-    trips: tripsFile,
-    network: networkFile,
-    output: routesFile,
-  })
+  if (fs.existsSync(routesFile)) {
+    console.log("Routes file already exists")
+  } else {
+    await convertTripsToRoutes({
+      trips: tripsFile,
+      network: networkFile,
+      output: routesFile,
+    })
+  }
+
   console.log("Done!\n")
 
   // Create visualization of routes for preview
   console.log("Creating a visualization of SUMO routes...")
-  await visualizeRoutes({
-    routes: routesFile,
-    network: networkFile,
-    output: routesVisualizationFile,
-  })
+  if (fs.existsSync(routesVisualizationFile)) {
+    console.log("Routes visualization file already exists")
+  } else {
+    await visualizeRoutes({
+      routes: routesFile,
+      network: networkFile,
+      output: routesVisualizationFile,
+    })
+  }
+
   console.log("Done!\n")
 
   // 3. Write SUMO config file
