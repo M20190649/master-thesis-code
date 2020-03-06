ca2e6082c73b75361093634d1b257a1934575456
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-03-06 16:41:35 +0100

    adjust archive download to download date ranges in timesteps

--- airdata/downloadLuftdatenInfo.js
+++ airdata/downloadLuftdatenInfo.js
@@ -152,4 +152,148 @@ async function downloadFromLuftdatenInfo(options) {
   return outputGeojson
 }
 
-module.exports = downloadFromLuftdatenInfo
+async function downloadFromLuftdatenInfoArchive(options) {
+  const pollutantMapping = {
+    PM10: "P1",
+    "PM2.5": "P2",
+  }
+
+  const pollutant = pollutantMapping[options.pollutant]
+
+  // API does not allow to filter the data for timestamps
+  // So I need to access the archives and filter manually
+
+  // Make a request to the API and get the latest measurements for the given bbox
+  const baseUrl = "http://data.sensor.community/airrohr/v1/filter"
+  const apiURL = `${baseUrl}/box=${options.bbox.join("%2C")}`
+  const { data: allLatestMeasurements } = await axios.get(apiURL)
+  // We filter all measurements for all the unique ones that report PM values (or other given pollutant)
+  const pmMeasurements = allLatestMeasurements
+    .filter(m => {
+      return m.sensordatavalues.some(v => {
+        return v.value_type === pollutant
+        // return v.value_type === "P1" || v.value_type === "P2"
+      })
+    })
+    .reduce((uniqueMeasurements, curr) => {
+      if (!uniqueMeasurements.map(m => m.sensor.id).includes(curr.sensor.id)) {
+        uniqueMeasurements.push(curr)
+      }
+      return uniqueMeasurements
+    }, [])
+
+  // Now we know which sensors report PM values in the given bbox
+  // Now we access the archives for all of these sensors for the given data
+
+  /*
+  Measurements object will contain a list of sensors with averages values for every timestep
+  {
+    '2020-02-20T00:00:30.000Z': [ 
+      { 
+        id: '582473be40198a001010e1a3',
+        value: 9.844444444444445,
+        location: {
+          latitude: Y,
+          longitude: X
+        }
+      },
+      ...
+    ]
+    '2020-02-20T01:00:00.000Z': [ 
+      { 
+        id: '58fb1549635722001156933b',
+        value: 8.335,
+        location: {
+          latitude: Y,
+          longitude: X
+        }
+      },
+      ...
+    ]
+  }
+  */
+  const measurements = {}
+
+  for (const m of pmMeasurements) {
+    let requestURL = ""
+    try {
+      requestURL = [
+        "http://archive.luftdaten.info/",
+        `${getDateString(options.from)}/`,
+        `${getDateString(options.from)}_`,
+        `${m.sensor.sensor_type.name.toLowerCase()}_sensor_${m.sensor.id}.csv`,
+      ].join("")
+      // Archives return CSV data
+      // We parse it into an array of measurement objects
+      const { data: csv } = await axios.get(requestURL)
+      const rows = csv.split("\n")
+      const header = rows.shift().split(";")
+
+      let currentTimeStep = new Date(options.from.getTime() + options.timestep * 60 * 1000)
+      let sum = 0
+      let counter = 0
+      for (const row of rows) {
+        const rowData = {}
+        row.split(";").forEach((value, i) => {
+          rowData[header[i]] = value
+        })
+
+        const tsDate = new Date(`${rowData.timestamp}Z`)
+        if (tsDate <= currentTimeStep) {
+          // When measurement value is within current timestep add it to sum
+          sum += parseFloat(rowData[pollutant])
+          counter++
+        } else {
+          // We reached the end of the time step
+          // Calculate the average and add it to the result object
+          if (sum > 0 && counter > 0) {
+            if (measurements[currentTimeStep.toISOString()]) {
+              measurements[currentTimeStep.toISOString()].push({
+                id: m.sensor.id,
+                value: sum / counter,
+                location: {
+                  latitude: parseFloat(m.location.latitude),
+                  longitude: parseFloat(m.location.longitude),
+                },
+              })
+            } else {
+              measurements[currentTimeStep.toISOString()] = [
+                {
+                  id: m.sensor.id,
+                  value: sum / counter,
+                  location: {
+                    latitude: parseFloat(m.location.latitude),
+                    longitude: parseFloat(m.location.longitude),
+                  },
+                },
+              ]
+            }
+          }
+
+          currentTimeStep = new Date(currentTimeStep.getTime() + options.timestep * 60 * 1000)
+          sum = 0
+          counter = 0
+
+          if (currentTimeStep > options.to) {
+            break
+          }
+        }
+      }
+    } catch (error) {
+      // console.log(requestURL)
+      // console.log(error.message)
+    }
+  }
+
+  console.log("Luftdaten.info")
+  console.log(`All measurements in bbox: ${allLatestMeasurements.length}`)
+  console.log(`PM Measurements in bbox: ${pmMeasurements.length}`)
+  console.log(
+    `Available PM Measurements in bbox at given time: ${Object.values(measurements)[0].length}`
+  )
+  console.log()
+
+  return measurements
+}
+
+module.exports = { downloadFromLuftdatenInfo, downloadFromLuftdatenInfoArchive }
