138ef88dbfb16e4d97d55d67311bb89a7225227e
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-03-10 10:53:40 +0100

    write to correct output directory

--- airdata/validation.js
+++ airdata/validation.js
@@ -1,15 +1,16 @@
-const { pad, runBash } = require("../shared/helpers")
+const { runBash } = require("../shared/helpers")
 const getAirData = require("./getAirData")
 
 async function run() {
-  const filenames = await getAirData({
+  const options = {
     pollutant: "PM10",
     bbox: "52.25639,12.874603,52.778678,13.932037".split(",").map(Number),
-    from: new Date(`2020-02-20T00:00:00.000Z`),
-    to: new Date(`2020-02-20T01:00:00.000Z`),
+    from: new Date(`2020-02-20T13:00:00.000Z`),
+    to: new Date(`2020-02-20T15:00:00.000Z`),
     timestep: 30,
-    output: "test",
-  })
+    output: "test-no-holes",
+  }
+  const filenames = await getAirData(options)
 
   console.log(filenames)
 
@@ -17,7 +18,7 @@ async function run() {
 
   for (const method of methods) {
     for (const file of filenames) {
-      const dirName = `test/${method}`
+      const dirName = `${options.output}/${method}`
       await runBash(
         `python interpolate.py --measurements=${file} --method=${method} --output=${dirName} --visualize=true`
       )
