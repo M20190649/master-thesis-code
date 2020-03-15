2032b9fe6a038347e9c5b8601f2c534be6a48f41
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-03-15 11:31:35 +0100

    add additional data to simConfig at start of simulation

--- sumo/traci/index.py
+++ sumo/traci/index.py
@@ -1,4 +1,4 @@
-import os, sys, getopt, json
+import os, sys, getopt, json, math
 
 from argparse import ArgumentParser
 
@@ -28,12 +28,16 @@ else:
 with open(args.config) as configPath:
     simConfig = json.load(configPath)
 
+# Add some additional data to simulation config dictionary
+simDir = os.path.dirname(os.path.realpath(args.config))
+simConfig["sim_airDataDir"] = os.path.join(simDir, simConfig["name"], "airdata")
+
 # sumoBinary = os.environ["SUMO_HOME"] + "/bin/sumo"
 sumoBinary = os.environ["SUMO_HOME"] + "/bin/sumo-gui"
 sumoCmd = [sumoBinary, "--configuration-file", args.sumoConfig]
 
 from controller import SimController
 
-traciConfig = {"sumoCmd": sumoCmd, "steps": 500}
+traciConfig = {"sumoCmd": sumoCmd, "steps": math.inf}
 controller = SimController(traciConfig, simConfig)
 controller.start()
