413efebdb2ab069d56be85cee9e2309d94401108
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-03-31 20:27:36 +0200

    add cross validation

---
+++ airdata/validation.py
@@ -0,0 +1,105 @@
+import random, pprint, glob
+from argparse import ArgumentParser
+import geopandas as gpd
+import numpy as np
+from scipy.spatial import cKDTree
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+import interpolators
+
+plt.rcParams["figure.figsize"] = 30, 20
+plt.rcParams["font.size"] = 20
+plt.rcParams["axes.titlesize"] = 50
+plt.rcParams["axes.titlepad"] = 80
+
+interpolator_functions = {
+    "nearest_neighbor": interpolators.nearest_neighbor,
+    "discrete_natural_neighbor": interpolators.discrete_natural_neighbor,
+    # "metpy_natural_neighbor": interpolators.metpy_natural_neighbor,
+    # "scipy_natural_neighbor": interpolators.scipy_natural_neighbor,
+    "idw": interpolators.inverse_distance_weighting,
+    "radial_basis_function": interpolators.radial_basis_function,
+    "scipy_radial_basis_function": interpolators.scipy_radial_basis_function
+    # "linear_barycentric": interpolators.linear_barycentric,
+}
+
+def cross_validate_file(measurements_file):
+    measurements = gpd.read_file(measurements_file)
+
+    external_crs = "EPSG:4326"
+    internal_crs = "EPSG:3068"
+
+    measurements = measurements.to_crs(internal_crs)
+
+    x = np.array(measurements.geometry.x)
+    y = np.array(measurements.geometry.y)
+    values = np.array(measurements.value)
+
+    xmin, ymin, xmax, ymax = measurements.total_bounds
+    size = 100  # grid cell size in meters
+    xnew = np.linspace(xmin, xmax, int((xmax - xmin) / size))
+    ynew = np.linspace(ymin, ymax, int((ymax - ymin) / size))
+
+    folds = 10
+    seed = random.randint(0,9999)
+    kfold = KFold(folds, True, seed)
+    avg_rmse_per_method = {}
+    for method in interpolator_functions:
+        interpolation_method = interpolator_functions[method] 
+        sum_rmse = 0
+        for train, test in kfold.split(values):
+            train_points = np.column_stack((x[train], y[train]))
+            train_values = values[train]
+
+            interpolated_values = interpolation_method(x[test], y[test], train_points, train_values, grid=False)
+            rmse = mean_squared_error(values[test], interpolated_values)
+            sum_rmse +=rmse
+        
+        avg_rmse= sum_rmse/folds
+        avg_rmse_per_method[method] = avg_rmse
+
+    return avg_rmse_per_method
+
+# cross_validate_file("./validation-02-02-2020/data_2020-02-02T08-00-00.geojson")
+
+def cross_validation(directory):
+  files = glob.glob(f"{directory}/*.geojson")
+
+  win_counter = dict.fromkeys(interpolator_functions.keys(), 0)
+  for file in files:
+    result = cross_validate_file(file)
+    # pprint.pprint(result)
+    winning_method = min(result, key=result.get)
+    win_counter[winning_method] += 1
+  
+  pprint.pprint(win_counter)
+  fig, ax = plt.subplots()
+  x_pos = np.arange(0, len(win_counter.keys()))
+  heights = list(win_counter.values())
+  labels = list(win_counter.keys())
+
+  ax.bar(x_pos, heights)
+  ax.set_xticks(x_pos)
+  ax.set_xticklabels(labels)
+  ax.set_ylabel("No. of wins")
+  ax.set_title("Interpolation method cross-validation")
+
+  for i, h in enumerate(heights):
+    margin = np.max(heights)*0.02
+    ax.text(x=i, y=h+margin, s=f"{h}", va="center", color='black', fontweight='bold')
+  
+  fig.savefig(f"{directory}/wins.png")
+    
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--directory",
+    dest="directory",
+    help="Directory to all GeoJSON measurement files",
+    metavar="FILE",
+)
+args = parser.parse_args()
+
+cross_validation(args.directory)
