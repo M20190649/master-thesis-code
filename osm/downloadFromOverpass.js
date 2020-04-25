601cf367397a185835f5d3abed91d818cd50ea73
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-04-25 17:16:51 +0200

    add service parameter. cleanup.

--- osm/downloadFromOverpass.js
+++ osm/downloadFromOverpass.js
@@ -12,6 +12,12 @@ const optionDefinitions = [
       "Bbox string (south,west,north,east) describing the boundaries which will be downloaded",
     required: true,
   },
+  {
+    name: "service",
+    type: Boolean,
+    description: "Query OSM with or without the road type 'service'",
+    defaultValue: false,
+  },
   { name: "output", type: String, description: "Filepath for the output XML file" },
 ]
 const CLIOptions = parseCLIOptions(optionDefinitions)
@@ -21,16 +27,30 @@ async function downloadFromOverpass(callerOptions) {
 
   validateOptions(options, optionDefinitions)
 
-  const queryWithoutServiceRoads = `(way["highway"~"motorway|trunk|primary|secondary|tertiary|residential|living_street|motorway_link|motorway_link|trunk_link|primary_link|secondary_link|tertiary_link|road|unclassified"](south,west,north,east);>;);out;`
+  const roadTypes = [
+    "motorway",
+    "trunk",
+    "primary",
+    "secondary",
+    "tertiary",
+    "residential",
+    "living_street",
+    "motorway_link",
+    "motorway_link",
+    "trunk_link",
+    "primary_link",
+    "secondary_link",
+    "tertiary_link",
+    "road",
+    "unclassified",
+  ]
 
-  // Files are significantly larger with service roads
-  const queryWithServiceRoads = `(way["highway"~"motorway|trunk|primary|secondary|tertiary|residential|living_street|motorway_link|motorway_link|trunk_link|primary_link|secondary_link|tertiary_link|service|road|unclassified"](south,west,north,east);>;);out;`
+  if (options.service) {
+    // Files are significantly larger with service roads but a majority of service roads are useless for most of the cars
+    roadTypes.push("service")
+  }
 
-  const query = queryWithoutServiceRoads
-
-  // const testBbox = [52.5056, 13.3075, 52.5182, 13.344] // south,west,north,east
-  // const greaterBerlinBbox = [52.25639, 12.874603, 52.778678, 13.932037] // south,west,north,east
-  // const matsimBbox = [50.819395, 11.227191, 54.321129, 15.241843]
+  const query = `(way["highway"~"${roadTypes.join("|")}"](south,west,north,east);>;);out;`
 
   const outputStream = fs.createWriteStream(options.output || "./road-network.osm.xml")
   await axios
