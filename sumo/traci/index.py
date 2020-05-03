66b2a6e033e4dcac16fe2d0e1ecf7d3f9a7097e9
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-05-03 14:05:43 +0200

    add more options to sumo cmd. formatting.

--- sumo/traci/index.py
+++ sumo/traci/index.py
@@ -16,11 +16,7 @@ parser.add_argument(
     metavar="FILE",
 )
 parser.add_argument(
-    "--gui",
-    dest="gui",
-    help="Run simulation in SUMO GUI",
-    metavar="BOOLEAN",
-    nargs='?'
+    "--gui", dest="gui", help="Run simulation in SUMO GUI", metavar="BOOLEAN", nargs="?"
 )
 
 args = parser.parse_args()
@@ -38,7 +34,9 @@ with open(args.config) as configPath:
 # Add some additional data to simulation config dictionary
 sim_dir = os.path.dirname(os.path.realpath(args.config))
 sim_name = re.search(".*(?=\.)", os.path.basename(args.config)).group(0)
-sim_config["sim_airDataDir"] = os.path.join(sim_dir, sim_name, "airdata", sim_config["interpolationMethod"])
+sim_config["sim_airDataDir"] = os.path.join(
+    sim_dir, sim_name, "airdata", sim_config["interpolationMethod"]
+)
 sim_config["sim_outputDir"] = os.path.join(sim_dir, sim_name, "output")
 
 sumo_binary = os.environ["SUMO_HOME"] + "/bin/sumo"
@@ -46,7 +44,17 @@ sumo_binary = os.environ["SUMO_HOME"] + "/bin/sumo"
 if args.gui is not None:
     sumo_binary = os.environ["SUMO_HOME"] + "/bin/sumo-gui"
 
-sumo_cmd = [sumo_binary, "--configuration-file", args.sumo_config]
+sumo_cmd = [
+    sumo_binary,
+    "--configuration-file",
+    args.sumo_config,
+    "--log",
+    os.path.join(sim_config["sim_outputDir"], "logs.txt"),
+    # "--device.rerouting.probability",
+    # "1",
+    # "--device.rerouting.period",
+    # "300"
+]
 
 from simulation_controller import SimulationController
 
