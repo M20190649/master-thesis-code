cd58317e72dafd3b409548bfd0ba06c17c8ddebb
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-05-09 09:31:07 +0200

    add more conversion options

--- sumo/convertOSMNetwork.js
+++ sumo/convertOSMNetwork.js
@@ -24,11 +24,14 @@ async function convertOSMNetwork(callerOptions) {
     // Recommended OSM conversion options
     "--geometry.remove",
     "--ramps.guess",
+    "--roundabouts.guess",
     "--junctions.join",
     "--tls.guess-signals",
     "--tls.discard-simple",
     "--tls.join",
     "--tls.default-type actuated",
+    "--output.original-names",
+    "--output.street-names",
     // DLR used this one in their simulation
     // http://sumo-user-mailing-list.90755.n8.nabble.com/sumo-user-Error-The-vehicle-type-type1-for-vehicle-veh0-is-not-known-tp3376p3390.html
     // https://sumo.dlr.de/docs/Networks/Import/OpenStreetMap.html#importing_large_networks
