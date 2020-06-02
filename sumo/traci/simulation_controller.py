0ce8048050d25f560081d9bf3cfe255b90267bcd
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-06-03 00:35:37 +0200

    add detailed time tracking

--- sumo/traci/simulation_controller.py
+++ sumo/traci/simulation_controller.py
@@ -28,24 +28,48 @@ class SimulationController:
         interval = self.sim_config["zoneUpdateInterval"] * 60
 
         t = time.time()
+        step_time = 0
+        prep_time = 0
+        tracking_time = 0
+        rerouting_time = 0
+
         # Run the simulation
         step = 0
         while step < 24 * 60 * 60 or traci.simulation.getMinExpectedNumber() > 0:
             # log(f"Before step {step}")
+            x = time.time()
             traci.simulationStep(step)
+            step_time += time.time() - x
             # log(f"After step {step}")
+
+            x = time.time()
             self.vehicle_controller.prepare_new_vehicles()
+            prep_time += time.time() - x
             # log(f"After new vehicle prep")
 
+            x = time.time()
             self.tracker.track_vehicles_in_polygons(step)
+            tracking_time += time.time() - x
             # log(f"After tracking")
 
             if step > 0 and step % interval == 0:
-                log(
-                    f"\nPrevious timestep simulation time: {format(time.time() - t, '.3f')}s\n"
+                prev_timestep = self.zone_controller.get_timestep_from_step(
+                    step - interval
                 )
+                curr_timestep = self.zone_controller.get_timestep_from_step(step)
+                log(
+                    f"\nPrevious timestep ({prev_timestep} - {curr_timestep}) simulation time: {format(time.time() - t, '.3f')}s"
+                )
+                log(f"Simulation step time: {format(step_time, '.3f')}s")
+                log(f"Vehicle preparation time: {format(prep_time, '.3f')}s")
+                log(f"Vehicle tracking time: {format(tracking_time, '.3f')}s")
+                log(f"Vehicle rerouting time: {format(rerouting_time, '.3f')}s")
+                log()
                 t = time.time()
-
+                step_time = 0
+                prep_time = 0
+                tracking_time = 0
+                rerouting_time = 0
                 # if step == interval * 4:
                 #     sys.exit()
 
@@ -53,7 +77,9 @@ class SimulationController:
                 # log(f"After zone update")
 
             if self.sim_config["zoneRerouting"] != "none":
+                x = time.time()
                 self.vehicle_controller.reroute()
+                rerouting_time += time.time() - x
                 # log(f"After reroute")
 
             step += 1
