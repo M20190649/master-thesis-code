8e956430de6ae3bea416efb4350f9faa4d7af059
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-06-17 03:55:51 +0200

    further speed optimization

--- sumo/traci/create_poly_db.py
+++ sumo/traci/create_poly_db.py
@@ -1,4 +1,4 @@
-import os, multiprocessing, time, functools
+import os, multiprocessing, time, functools, uuid
 from argparse import ArgumentParser
 import sqlite3
 import sumolib, traci
@@ -39,19 +39,54 @@ c.execute(f"CREATE TABLE IF NOT EXISTS polygons ({table_string})")
 get_values = lambda p: [f"'{p[attrib['name']]}'" for attrib in attribs]
 
 
-def find_edges(start, file_path):
-    t = time.time()
-    polygons = []
-    for event, element in etree.iterparse(file_path, tag="poly"):
-        p = {
-            "id": element.attrib["id"],
-            "zone": int(element.attrib["id"].split("-")[-2]),
-            "timestep": os.path.basename(file_path).split(".")[0].split("T")[1],
-            "color": element.attrib["color"],
-            "layer": int(float(element.attrib["layer"])),
-            "shape": element.attrib["shape"],
-        }
-        polygons.append(p)
+def process_files(start, files):
+    def get_polygons(file_path):
+        t = time.time()
+        polygons = []
+        for event, element in etree.iterparse(file_path, tag="poly"):
+            p = {
+                "id": element.attrib["id"],
+                "zone": int(element.attrib["id"].split("-")[-2]),
+                "timestep": os.path.basename(file_path).split(".")[0].split("T")[1],
+                "color": element.attrib["color"],
+                "layer": int(float(element.attrib["layer"])),
+                "shape": element.attrib["shape"],
+            }
+            polygons.append(p)
+
+        for p in polygons:
+            pid = p["id"]
+            shape = list(
+                map(
+                    lambda pair: tuple(map(float, pair.split(","))),
+                    p["shape"].split(" "),
+                )
+            )
+            color = list(map(int, p["color"].split(",")))
+            traci.polygon.add(pid, shape, color)
+            traci.polygon.subscribeContext(
+                pid, tc.CMD_GET_EDGE_VARIABLE, 0, [tc.ID_COUNT]
+            )
+            polygon_context = traci.polygon.getContextSubscriptionResults(pid)
+            traci.polygon.unsubscribeContext(pid, tc.CMD_GET_EDGE_VARIABLE, 0)
+
+            edges_in_polygon = []
+            if polygon_context is not None:
+                edges_in_polygon = list(polygon_context.keys())
+
+            p["edges"] = " ".join(edges_in_polygon)
+
+            traci.polygon.remove(pid)
+
+            print(p["id"], len(p["edges"].split(" ")), "edges")
+            values = get_values(p)
+            c.execute(f"INSERT INTO polygons VALUES ({','.join(values)})")
+            conn.commit()
+
+        print(
+            "Done", os.path.basename(file_path), f"({format(time.time() - t, '.3f')}s)"
+        )
+        print("Total time:", format(time.time() - start, ".3f"), "s")
 
     sumo_binary = os.environ["SUMO_HOME"] + "/bin/sumo"
     sumo_cmd = [
@@ -61,36 +96,12 @@ def find_edges(start, file_path):
         "--no-warnings",
         "--no-step-log",
     ]
-    traci.start(sumo_cmd, label=file_path)
+    traci.start(sumo_cmd, label=uuid.uuid4())
 
-    for p in polygons:
-        pid = p["id"]
-        shape = list(
-            map(lambda pair: tuple(map(float, pair.split(","))), p["shape"].split(" "),)
-        )
-        color = list(map(int, p["color"].split(",")))
-        traci.polygon.add(pid, shape, color)
-        traci.polygon.subscribeContext(pid, tc.CMD_GET_EDGE_VARIABLE, 0, [tc.ID_COUNT])
-        polygon_context = traci.polygon.getContextSubscriptionResults(pid)
-        traci.polygon.unsubscribeContext(pid, tc.CMD_GET_EDGE_VARIABLE, 0)
-
-        edges_in_polygon = []
-        if polygon_context is not None:
-            edges_in_polygon = list(polygon_context.keys())
-
-        p["edges"] = " ".join(edges_in_polygon)
-
-        traci.polygon.remove(pid)
-
-        print(p["id"], len(p["edges"].split(" ")), "edges")
-        values = get_values(p)
-        c.execute(f"INSERT INTO polygons VALUES ({','.join(values)})")
-        conn.commit()
+    for file in files:
+        get_polygons(file)
 
     traci.close()
-    print("Done", os.path.basename(file_path), f"({format(time.time() - t, '.3f')}s)")
-    print("Total time:", format(time.time() - start, ".3f"), "s")
-    return
 
 
 if __name__ == "__main__":
@@ -99,10 +110,17 @@ if __name__ == "__main__":
     path = base_dir + "/airdata/PM10-idw"
     files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".xml")]
 
+    def chunks(l, n):
+        n = max(1, n)
+        return [l[i : i + n] for i in range(0, len(l), n)]
+
+    files_per_process = 5
+    files = chunks(files, files_per_process)
+
     start = time.time()
 
     pool = multiprocessing.Pool()
-    func = functools.partial(find_edges, start)
+    func = functools.partial(process_files, start)
     pool.map(func, files)
     pool.close()
     pool.join()
