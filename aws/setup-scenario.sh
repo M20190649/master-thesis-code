b75efceb3a5ef61f09857756f94424b80fa78f08
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-06-19 01:26:20 +0200

    adjust script

--- aws/setup-scenario.sh
+++ aws/setup-scenario.sh
@@ -1,4 +1,21 @@
 mkdir scenarios/$1
 
-cp -r demand scenarios/$1/demand
-cp -r network scenarios/$1/network
+cp -r scenarios/N/network scenarios/$1/network
+cp -r scenarios/N/demand scenarios/$1/demand
+
+mkdir scenarios/$1/airdata
+
+if [[ ${1:0:1} == "L" ]]; then
+  cp -r light_25-03-2020/PM10-raw scenarios/$1/airdata/PM10-raw
+  cp -r light_25-03-2020/PM10-idw scenarios/$1/airdata/PM10-idw
+fi
+
+if [[ ${1:0:1} == "M" ]]; then 
+  cp -r medium_11-06-2020/PM10-raw scenarios/$1/airdata/PM10-raw
+  cp -r medium_11-06-2020/PM10-idw scenarios/$1/airdata/PM10-idw
+fi
+
+if [[ ${1:0:1} == "H" ]]; then 
+  cp -r heavy_24-01-2020/PM10-raw scenarios/$1/airdata/PM10-raw
+  cp -r heavy_24-01-2020/PM10-idw scenarios/$1/airdata/PM10-idw
+fi
