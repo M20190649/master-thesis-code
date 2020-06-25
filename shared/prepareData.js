e1e0516b3df78bfd47a63e7bf0e177f9783a4c6e
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-06-25 10:32:40 +0200

    adjust data prep

--- shared/prepareData.js
+++ shared/prepareData.js
@@ -1,4 +1,5 @@
-const fs = require("fs")
+const { join } = require("path")
+const glob = require("glob")
 
 const downloadMATSimNetwork = require("../network/matsim/downloadMATSimNetwork")
 const downloadMATSimPlans = require("../demand/downloadMATSimPlans")
@@ -10,20 +11,26 @@ const run = async () => {
   console.log()
 
   const scenarios = ["1pct", "10pct"]
+  const plansDir = join(__dirname, "..", "demand", "plans")
 
   for (const s of scenarios) {
-    const plansFile = await downloadMATSimPlans({
-      scenario: s,
-    })
+    const tripFileSearchPattern = `*${s}*trips.xml`
+    const searchPattern = join(plansDir, tripFileSearchPattern)
+    const [tripsFile] = glob.sync(searchPattern)
 
-    console.log()
-
-    const tripsFile = plansFile.replace(".xml", ".trips.xml")
-
-    console.log(`Convert MATSim ${s} plans to SUMO trips...`)
-    if (fs.existsSync(tripsFile)) {
-      console.log(`${s} trips file already exists`)
+    console.log(`Checking for ${s} trips file...`)
+    if (tripsFile) {
+      console.log(`${s} trips file already exists\n`)
     } else {
+      console.log(`Convert MATSim ${s} plans to SUMO trips...`)
+      const plansFile = await downloadMATSimPlans({
+        scenario: s,
+      })
+
+      const tripsFile = plansFile.replace(".xml", ".trips.xml")
+
+      console.log()
+
       await convertPlansToTrips({
         plans: plansFile,
         mode: "geo",
