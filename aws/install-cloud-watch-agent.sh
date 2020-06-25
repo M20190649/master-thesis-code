2c8a126e1228d94cd3434f136e57f95fa364ec6e
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-06-25 11:13:38 +0200

    remove deb package

--- aws/install-cloud-watch-agent.sh
+++ aws/install-cloud-watch-agent.sh
@@ -1,5 +1,6 @@
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo dpkg -i -E amazon-cloudwatch-agent.deb
+sudo rm amazon-cloudwatch-agent.deb
 
 sudo apt-get update
 sudo apt-get install collectd
